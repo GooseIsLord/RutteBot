da43a4c0bab40f7476d5a1bea11b15b8aff328f1
AnnevanVelden <annevanvelden@hotmail.com>
2020-08-09 12:26:50 +0200

    upgrade to discord.js v12 and Caroliene clause added

--- rutte.js
+++ rutte.js
@@ -17,7 +17,7 @@ bot.on('ready', async () => {
 });
 
 // if a message is sent
-bot.on('message', async (message) => {
+bot.on('message', async message => {
 	var con = mysql.createConnection({
 		host: botConfig.host,
 		user: botConfig.user,
@@ -33,9 +33,9 @@ bot.on('message', async (message) => {
 		return;
 	}
 
-	if (message.content == '*help') {
+	if (message.content === '*help') {
 		return message.channel.send(
-			"The bot is simple. For every message you sent there is a 1/10 chance you get a random (and publicly available) picture from the prime minister of the Netherlands: Mark Rutte. \n If you either use: Rutte, Mark, Markie or one of the secret words in your message the chances will go up to 1/5. \n The bot draws from a database of 250+ pictures that it randomly chooses from every time it gets a chance to react to you. \n It's a silly bot for good reaction immages that you can have fun with."
+			"The bot is simple. For every message you sent there is a 1/10 chance you get a random (and publicly available) picture from the prime minister of the Netherlands: Mark Rutte. \nIf you either use: Rutte, Mark, Markie or one of the secret words in your message the chances will go up to 1/5. \nThe bot draws from a database of 250+ pictures that it randomly chooses from every time it gets a chance to react to you. \nIt's a silly bot for good reaction images that you can have fun with. \nWant more Rutte in your server? Use this link to invite him: https://discord.com/oauth2/authorize?client_id=692139313848254564&scope=bot."
 		);
 	} else {
 		var rutte = message.content.includes('rutte');
@@ -70,35 +70,39 @@ bot.on('message', async (message) => {
 		var Vader = message.content.includes('Vader');
 		var VADER = message.content.includes('VADER');
 
+		var caroliene = message.content.includes('caroliene');
+		var Caroliene = message.content.includes('Caroliene');
+		var CAROLIENE = message.content.includes('CAROLIENE');
+
 		if (
-			rutte == true ||
-			Rutte == true ||
-			RUTTE == true ||
-			mark == true ||
-			Mark == true ||
-			MARK == true ||
-			markie == true ||
-			Markie == true ||
-			MARKIE == true ||
-			markje == true ||
-			Markje == true ||
-			MARKJE == true ||
-			daddy == true ||
-			Daddy == true ||
-			DADDY == true ||
-			gekoloniseerd == true ||
-			Gekoloniseerd == true ||
-			GEKOLONISEERD == true ||
-			papa == true ||
-			Papa == true ||
-			PAPA == true ||
-			vader == true ||
-			Vader == true ||
-			VADER == true
+			rutte === true ||
+			Rutte === true ||
+			RUTTE === true ||
+			mark === true ||
+			Mark === true ||
+			MARK === true ||
+			markie === true ||
+			Markie === true ||
+			MARKIE === true ||
+			markje === true ||
+			Markje === true ||
+			MARKJE === true ||
+			daddy === true ||
+			Daddy === true ||
+			DADDY === true ||
+			gekoloniseerd === true ||
+			Gekoloniseerd === true ||
+			GEKOLONISEERD === true ||
+			papa === true ||
+			Papa === true ||
+			PAPA === true ||
+			vader === true ||
+			Vader === true ||
+			VADER === true
 		) {
 			var randomRutte = Math.floor(Math.random() * 5) + 1;
 
-			if (randomRutte == 1) {
+			if (randomRutte === 1) {
 				con.query(`SELECT * FROM mark`, function (err, rows) {
 					// if there is a error, throw it my way
 					if (err) {
@@ -112,6 +116,19 @@ bot.on('message', async (message) => {
 					});
 				});
 			}
+		} else if (caroliene === true || Caroliene === true || CAROLIENE === true) {
+			con.query(`SELECT * FROM mark`, function (err, rows) {
+				// if there is a error, throw it my way
+				if (err) {
+					throw err;
+				}
+
+				var rand = Math.floor(Math.random() * rows.length);
+
+				return message.channel.send({
+					files: [rows[rand].link],
+				});
+			});
 		} else {
 			var random = Math.floor(Math.random() * 10) + 1;
 
